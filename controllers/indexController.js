ca586d62a2b202bb32376de1b618d5f4a68e930c
FacuCarbon <facundocarbon2015@gmail.com>
2021-02-11 00:40:51 -0300

    Nueva vista reiki

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -7,7 +7,7 @@ index: (req,res) =>{
 
 res.render('index',{
     title: 'Energia de luz - Inicio',
-    css: 'style.css'
+    css: 'index.css'
 })
     
 },
@@ -15,28 +15,28 @@ res.render('index',{
 masajes: (req,res) =>{
     res.render('masajes',{
         title: 'Energia de luz - masajes',
-        css: 'style.css'
+        css: 'masajes.css'
     })
 },
 
 reiki: (req,res) =>{
     res.render('reiki',{
         title: 'Energia de luz - reiki',
-        css: 'style.css'
+        css: 'reiki.css'
     })
 },
 
 tarot: (req,res) =>{
     res.render('tarot',{
         title: 'Energia de luz - tarot',
-        css: 'style.css'
+        css: 'tarot.css'
     })
 },
 
 contacto: (req,res) =>{
     res.render('contacto',{
         title: 'Energia de luz - contacto',
-        css: 'style.css'
+        css: 'contacto.css'
     })
 },
 
